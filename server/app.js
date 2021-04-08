a4326bdb2a175cc1f61c044a7dc7dee6fcb7a42f
omar-dahdouh <omar.m.dahdouh@gmail.com>
2021-04-09 00:15:58 +0300

    fix port number

--- server/app.js
+++ server/app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const routes = require('./router');
 
+app.set('port', process.env.PORT || 5000);
+
 app.use(express.static('public'));
 app.use(routes);
 
